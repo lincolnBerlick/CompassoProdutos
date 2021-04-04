c4214b1b82c2e1e3cb3f50880e045285218db512
Lincoln Berlick <lincoln@xbrain.com.br>
2021-04-04 13:21:07 -0300

    #remove flyway duplicado

--- src/test/java/com/example/demo/modules/produtos/service/ProductServiceTest.java
+++ src/test/java/com/example/demo/modules/produtos/service/ProductServiceTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
